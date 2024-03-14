41b7b1077f2a7038c73f8a0ffc5afbf220015d43
Robert <j12345678987654321@yandex.ru>
2024-03-14 14:10:11 +0500

    Add try except

--- helpers/unpickler.py
+++ helpers/unpickler.py
@@ -2,5 +2,8 @@ import os
 import pickle
 
 def load(path: str):
-    with open(os.path.join(path), "rb") as f:
-        return pickle.load(f)
+    try: 
+        with open(os.path.join(path), "rb") as f:
+            return pickle.load(f)
+    except:
+        return None

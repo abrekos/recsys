eea3ab219b9f235d8e64b180322c49b93e1f8b6d
Robert <j12345678987654321@yandex.ru>
2024-03-14 12:36:49 +0500

    Add model to webservice

--- service/api/models/models.py
+++ service/api/models/models.py
@@ -1,3 +1,11 @@
 from .simplelist import SimpleListModel
+from .userknnreco import UserKnn
+from helpers.unpickler import load
 
-models = {"simple_list_model": SimpleListModel()}
+
+models = {
+    "simple_list_model":  SimpleListModel(),
+    "user_knn": UserKnn(
+        backbone_model=load("userknn.pkl"),
+    )
+}

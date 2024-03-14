d75f22c022816c409fb40bea5e77d6bec8edf4a7
Robert <j12345678987654321@yandex.ru>
2024-03-14 19:13:35 +0500

    Task 4

--- service/api/models/models.py
+++ service/api/models/models.py
@@ -2,10 +2,24 @@ from helpers.unpickler import load
 
 from .simplelist import SimpleListModel
 from .userknnreco import UserKnn
+from .pop import PopModel
+from .ann import ANN
+
+ALS_MODEL_PATH = "weights/als.pkl"
+ANN_MODEL_PATH = "weights/ann.pkl"
+POP_MODEL_PATH = "weights/pop.pkl"
+DATASET_PATH = "kion_train/interactions.csv"
 
 models = {
     "simple_list_model": SimpleListModel(),
     "user_knn": UserKnn(
         backbone_model=load("weights/userknn.pkl"),
     ),
+
+    "als_model": ANN(
+            backbone_model=load(ANN_MODEL_PATH),
+            popular_model=PopModel(
+                dataset_path=DATASET_PATH, backbone_model=load(POP_MODEL_PATH)
+            ),
+        ),
 }

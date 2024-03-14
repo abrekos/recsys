aa7883a31ab40d890000d5520bd222fe24bc35e9
Robert <j12345678987654321@yandex.ru>
2024-03-14 12:45:29 +0500

    Fix path

--- service/api/models/models.py
+++ service/api/models/models.py
@@ -1,11 +1,11 @@
-from .simplelist import SimpleListModel
-from .userknnreco import UserKnn
 from helpers.unpickler import load
 
+from .simplelist import SimpleListModel
+from .userknnreco import UserKnn
 
 models = {
-    "simple_list_model":  SimpleListModel(),
+    "simple_list_model": SimpleListModel(),
     "user_knn": UserKnn(
-        backbone_model=load("userknn.pkl"),
-    )
+        backbone_model=load("weights/userknn.pkl"),
+    ),
 }
